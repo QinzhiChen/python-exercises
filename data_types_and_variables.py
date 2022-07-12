2d917d805bb54b3c5a9217b78abc67e98ade579d
QinzhiChen <qinzhic@gmail.com>
2022-07-12 16:52:44 -0500

    files modifed to correct couple answers input

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -1,5 +1,14 @@
-from contextlib import nullcontext
-from xmlrpc.client import boolean
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+type (17)
+
+
+# In[64]:
+
 
 print("Movies Rental")
 # rent little mermaid for 3 days, Brother Bear for 5 days, hercules for 1 day
@@ -7,8 +16,9 @@ print("Movies Rental")
 Mermaid = 3
 Brother_Bear = 5 
 Hercules =1
+price_rate=3
 print ("Total Day Rental:",Mermaid+Brother_Bear+Hercules,"Days")
-print ("Total Cost $", 3*Mermaid+Brother_Bear+Hercules,)
+print ("Total Cost $", price_rate*(Mermaid+Brother_Bear+Hercules))
 
 # working as contractor for 3 companies: Google, Amazon, and Facebook.
 # Which Google pays $400/hr, Amazon $380/hr, and Facebook $350/hr
@@ -17,41 +27,52 @@ print ("Weekly Salary")
 Google=480
 Amazon=380
 Facebook=350
-print("Total Salary This Week $", Google*6+Amazon*4+Facebook*10)
+print("Total Salary This Week $", (Google*6)+(Amazon*4)+(Facebook*10))
 
 # student can enroll if the class is not full or schedule is not conflict
 full_class=0
 schedule_conflict=0
-print("Enrollment", bool(full_class))
-print("Enrollment", bool(schedule_conflict))
+enrollment = full_class and schedule_conflict
+
 
 def item():
     if item > 2:
+        return ("offer applied")
+    if item("Premium Member"):
         return ("offer applied")
     else:
         return ("This Offer can't apply")
 
 username= 'codeup'
 password = 'notastrongpassword'
-def newp(password):
+def newp():
     if len(password)>5:
         return True
-    if len(username)<20:
+    elif len(username)<20:
         return True
-    if password!= username:
+    elif password!= username:
         return True
-    else:
+    elif password(" this".strip()):
         return False
-def newu(username):
-    if len(password)>5:
-        return True
-    if len(username)<20:
-        return True
-    if password!= username:
-        return True
+    elif username(" this".strip()):
+        return False
     else:
         return False
 
-print(newp("notastrongpassword"))
-print(newu('codeup'))
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
