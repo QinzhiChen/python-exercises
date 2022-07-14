8f833f53b5f9476f455f10031f20c57e10016881
QinzhiChen <qinzhic@gmail.com>
2022-07-14 15:44:15 -0500

    modified files and created a new file

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -1,41 +1,44 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[9]:
+# In[21]:
 
 
 # when today is Monday
-week = False
-if week:
+day=input('Enter the day here: ')
+if day == "Monday":
     print('it is Monday')
 else:
     print ('it is not Monday')
 
 
-# In[10]:
+# In[26]:
 
 
 #  when today is weekday
-weekday=True
-if weekday:
+weekday=input("Enter the day here: ")
+if weekday=="Monday"or weekday=="Tuesday"or weekday=='Wednesday'or weekday=='Thursday'or weekday=='Friday':
     print('it is weekday')
 else:
-    print('it is not weekday')
+    print('it is weekend')
 
 
-# In[1]:
+# In[49]:
 
 
 #  create a variable that calculate the total and when working hour greater than 40 the extra hours equal to 1.5 times hourly rate
-hour_worked=41
+hour_worked=float(input("How Many hours you work last week: "))
 hourly_rate=30
 total = hour_worked*hourly_rate
-if hour_worked>40:
-    total =(hour_worked-40)*(hourly_rate*1.5)+(40*hourly_rate)
-print(total)
+if 168>hour_worked > 40:
+    print(total =(hour_worked-40)*(hourly_rate*1.5)+(40*hourly_rate))
+if hour_worked<=40:
+    print(total =hour_worked*hourly_rate)
+if hour_worked>=168:
+    print('This many hours do not existed in a week yet, go back to your planet')
 
 
-# In[2]:
+# In[50]:
 
 
 # create a while loop when i =5 and i+1 while i is less and equal to 15
@@ -67,7 +70,7 @@ while i >= -10:
     i -= 5
 
 
-# In[16]:
+# In[37]:
 
 
 # alter loop to start with 2 and display the number square on each line while the number is less than 1,000000
@@ -92,13 +95,13 @@ while i >= 5:
 # In[36]:
 
 
-# creating a float/int for output to times from 1-10
+# creating a float or int for output to times from 1-10
 n=float(input())
 for i in range(1,11):
     print(n,'x',i,'=',i*n)
 
 
-# In[35]:
+# In[53]:
 
 
 # creating for loop with prescribed answer
@@ -108,29 +111,32 @@ for i in range(1,10):
     print()
 
 
-# In[114]:
+# In[84]:
 
 
 # Creating a for loop to skip ipput odd number
 n=int(input("Number to skip is: ")) 
 number=n
 for n in range(1,50):
-    if n%2!=0 and n!=number:
+    if n%2!=0 and (n!=number and n>=1):
         print("Here is an odd number: ",n)
     if number==n:
         print('Yikes!Skipping number: ',n)
         continue
         break
+if n ==str():
+    print("Please enter valid number")
+    
+     else:
+        print('Pick a number between 1-50')
 
 
-# In[209]:
+# In[94]:
 
 
 n=int(input("your input: "))
 for number in range(n+1):
     print(number)
-if n ==str():
-    print("Please enter valid number")
 
 
 # n=int(input("your input: "))
@@ -154,27 +160,27 @@ for n in range (1,101):
     print(n)
 
 
-# In[140]:
+# In[95]:
 
 
-number=int(input("Your input Number"))
+number=int(input("Your input Number: "))
 if number %3==0:
     print('Fizz')
 else:
     print(number)
 
 
-# In[143]:
+# In[96]:
 
 
-number=int(input("Your input Number"))
+number=int(input("Your input Number: "))
 if number %5==0:
     print('Buzz')
 else:
     print(number)
 
 
-# In[145]:
+# In[2]:
 
 
 number=int(input("Your input Number"))
@@ -184,7 +190,7 @@ else:
     print(number)
 
 
-# In[231]:
+# In[19]:
 
 
 from tabulate import tabulate
@@ -199,13 +205,13 @@ while True:
         break
 
 
-# In[235]:
+# In[11]:
 
 
 n=float(input('Please enter your grade: '))
 while True:
     if n>100:
-        print ('Please enter number between 1-100')
+        print ('Please enter number between 1-100, or 0 :(')
     if n<=100 and n>=88:
         print("Your letter grade is A")
     if n<88 and n>=80:
@@ -216,28 +222,30 @@ while True:
         print("Your letter grade is D")
     if n<60 and n>=0:
         print("Your letter grade is F")
-    n=int(input('enter number to continue, otherwise type"00" to stop'))
-    if n == 00:
+    n=int(input('enter number to continue, otherwise type"-1" to stop: '))
+    if n == int('-1'):
         print('Okay, Bye!')
         break
 
 
-# In[314]:
+# In[14]:
 
 
-book=dict({'title':'1984', 'author':'George Orwell','genre':"Dystopian Fiction"})
+book=[{'title':'1984', 'author':'George Orwell','genre':"Dystopian Fiction"},
+         {'title':'1985', 'author':'George Orwell II','genre':"Scientific Fiction"},
+         {'title':'1986', 'author':'George Orwell III','genre':"Social Science Fiction"}]
 print(book)
 
 
-# In[316]:
+# In[15]:
 
 
-book=[{'title':'1984', 'author':'George Orwell','genre':"Dystopian Fiction"}]
+book=[{'title':'1984', 'author':'George Orwell','genre':"Dystopian Fiction"},
+         {'title':'1985', 'author':'George Orwell II','genre':"Scientific Fiction"},
+         {'title':'1986', 'author':'George Orwell III','genre':"Social Science Fiction"}]
 b=input('Please enter the book genres: ')        
-while True:
-    if b=='Dystopian Fiction':
-        print(f"{book['title']} is your book")
-    b=input('Please enter the book genres: ')
+for b in book:
+    print(b)
 
 
 # In[ ]:
